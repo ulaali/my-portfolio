069fb2b14980a9ab0ec5a779e1c573066668b983
ulaali <ulaali513@yahoo.com>
2023-07-14 14:09:11 +0300

    update my portfolio

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ function validation() {
     }
   }
 
+
+  // let myear=1999;
+  // let d=new Date();
+  // let year=d.getFullYear();
+  // let age=year-myear;
+  // document.getElementById("demoo").innerHTML=`MY name is ula ali and Im ${age} Years old,gratuated from al-nahrain university ,IT engineering 1 year experience<br> in electronics sales and admirer of programming
+  // `;
+
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
+    
+
+let myVar;
+
+function myFunction() {
+
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
